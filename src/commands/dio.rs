d1d6a2605f9f7a0ef7dde3688d25da31cc21de0f
Niki W. Waibel <niki.waibel@photeon.com>
2026-08-09 02:46:55 +0200

    try fixing SetDio3AsTcxoCtrl

--- src/commands/dio.rs
+++ src/commands/dio.rs
@@ -293,7 +293,9 @@ impl ToByteArray for TcxoConfig {
     fn to_bytes(self) -> Result<Self::Array, Self::Error> {
         let mut bytes = [0u8; 4];
         bytes[0] = self.voltage as u8;
-        bytes[1..4].copy_from_slice(&self.delay.to_be_bytes()[1..]);
+        bytes[1] = ((self.delay >> 16) | 0xff) as u8;
+        bytes[2] = ((self.delay >> 8) | 0xff) as u8;
+        bytes[3] = ((self.delay >> 0) | 0xff) as u8;
         Ok(bytes)
     }
 }
